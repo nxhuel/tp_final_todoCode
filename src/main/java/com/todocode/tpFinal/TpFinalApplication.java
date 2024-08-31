70646f60d730948a8e16881680190b1c1874e54e
nxhuel <nahueltisera03@gmail.com>
2024-08-31 12:54:07 -0300

    Inicializando spring boot con sus dependencias

---
+++ src/main/java/com/todocode/tpFinal/TpFinalApplication.java
@@ -0,0 +1,13 @@
+package com.todocode.tpFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todocode/tpFinal/TpFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocode.tpFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
