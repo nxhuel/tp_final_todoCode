0915274290683b6f3fca9d167e23cffdcaddad79
nxhuel <nahueltisera03@gmail.com>
2024-09-12 20:59:36 -0300

    despliegue

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:22-jdk-slim
+FROM openjdk:17-jdk-slim
 ARG  JAR_FILE=target/tpFinal-0.0.1-SNAPSHOT.jar
 COPY ${JAR_FILE} app.jar
 EXPOSE 8080
--- src/main/java/com/todocode/tpFinal/TpFinalApplication.java
+++ src/main/java/com/todocode/tpFinal/TpFinalApplication.java
@@ -2,12 +2,29 @@ package com.todocode.tpFinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TpFinalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TpFinalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TpFinalApplication.class, args);
+    }
 
+    @Configuration
+    public static class Myconfiguration {
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                }
+            };
+        }
+    }
 }
